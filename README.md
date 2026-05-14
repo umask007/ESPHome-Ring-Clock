55211271e9c4cd7a99d836c6c62956506a830036
umask007 <stout@testudo.net>
2026-05-14 18:56:38 +0200

    added watchface inverted analog

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ This is also my project to learn about esphome.
 
 * [x] Using RTC, no loading time
 * [x] Works with a second (inner) ring for the hour hand
-* [x] Three animation types:
+* [x] Automatic brightness with light sensor
+* [x] Three watchface animation types:
     * [x] Classic Analog: the analog clock with ticks and a wide hour hand
     * [x] 5min Arc at Hand: shows a dim arc for the 5min interval for each hand
     * [x] Arc from Top to Hand: shows a dim arc from top to the hour/minute hands
+    * [x] Inverted Analog: two rings light up, hands are dark
 * [x] Settings in web\_server or HASS
-    * [x] Select animation type
+    * [x] Select watchplate animation type
     * [x] Select color palette
     * [x] Select colors and effects (not persistent)
     * [x] Enable/Disable fade out (not persistent)
@@ -20,12 +22,12 @@ This is also my project to learn about esphome.
 
 ## How to use
 
-* If you're not using HASS, follow guide to [Install ESPHome Manually](https://esphome.io/guides/installing_esphome)
+* If you're not using Home Assistant, follow guide to [Install ESPHome Manually](https://esphome.io/guides/installing_esphome)
 * Create a new ESPHome configuration YAML file using [esphome_ring_clock.yaml](./esphome_ring_clock.yaml) as a template
 * Customize the basic device configuration (Leds, Board, WiFi, pins, etc.) to your liking
-* Customize the fields in the `substitutions` part at the top
 * Customize the !secrets substitutions to match your secrets.yaml file
 * Customize color palettes
+* Without light sensor, remove the "sensor:" part and reduce default brightness to 50%.
 * Copy [ring_clock.h](./ring_clock.h).
 * Deploy and enjoy!
 
@@ -37,12 +39,13 @@ If you use two LED rings, the first 60 LEDs are used for minute and second hands
 
 My hardware:
 * Wemos D1 mini v4
-* ds1307 attached to wemos i2c connector
+* ds1307 attached with qwiic connector
+* bh1750 light sensor chained to rtc
 * RX pin to Din of 60 LED ring, Dout to Din of 48 LED ring.
 * usb-c powered from a 12W USB charger
 This is mounted in a picture frame (IKEA sannahed 25x25cm).
 LED rings are sandwiched between 4mm 25x25cm mounting board and 4mm 25x25cm Acrylcolor Gray (Gutta).
-The mounting board has holes for LED ring connections, all other electronics are mounted to the back of that board.
+The mounting board has holes for LED ring connections and light sensor, all other electronics are mounted to the back of that board.
 
 ## Credits
 
--- ring_clock.h
+++ ring_clock.h
@@ -2,7 +2,6 @@ void update_clock_hands(AddressableLight &it, ESPTime time,
                         Color clock_ring_colors[], int offset[], Color colors[]) {
   static boolean initialized;
   int num_leds = it.size();
-//FIXME: allow specifying ring size ring[], eg: {60,48}
 
 /*
 esphome globals used:
@@ -12,9 +11,13 @@ esphome globals used:
     0: classic analog clock
     1: paint dim arcs of 5 min
     2: paint dim arcs from top
+    3: classic inverse minutes
    id(clock_seconds_enabled) bool
-
 */
+#define MODE_ANALOG 0
+#define MODE_ARC5 1
+#define MODE_ARC12 2
+#define MODE_AINV 3
 
   if (initialized == false) {
     std::fill_n(clock_ring_colors, it.size(), Color::BLACK);
@@ -25,116 +28,113 @@ esphome globals used:
     it[0]=Color(0xfffffff); // all hands at 12
     return;
   }
-  // calculate HAND indices
-  // if size is <=60, assuming single ring of that size
-  int second_idx = (int)(time.second * num_leds / 60.0);
-  int minute_idx = (int)(time.minute * num_leds / 60.0);
-  int hour_idx   = (int)(0.5f+(time.hour%12 * 60 + time.minute) * num_leds / 720.0f);
-  int hour_1_idx = (hour_idx + num_leds -1 ) % num_leds;
-  int hour_2_idx = (hour_idx             +1 ) % num_leds;
+
+  // calculate hand indices
+  // if num_leds is <=60, assuming single ring of that size
   int r1_size=num_leds;
-  int r2_size=0;
-  // if size is >60, assuming outer ring size 60 and inner ring of remaining leds
-  // re-calculate HAND indices if there is an inner ring
+  int r2_size=num_leds;
+  int r2_base=0;
+  // if num_leds is >60, assuming outer ring size 60 and inner ring of remaining leds
   if (r1_size > 60) {
     r1_size=60;
     r2_size=num_leds-r1_size;
-    second_idx = (int)time.second;
-    minute_idx = (int)(time.minute);
-    // hours on second ring
-    hour_idx   = (int)(0.5f+(time.hour%12 * 60 + time.minute) * r2_size / 720.0f) + 60;
-    hour_1_idx = (hour_idx -61 + r2_size ) % r2_size + 60;
-    hour_2_idx = (hour_idx -59           ) % r2_size + 60;
+    r2_base=r1_size;
   }
+
+  int second_idx = (int)(time.second * r1_size / 60.0);
+  int minute_idx = (int)(time.minute * r1_size / 60.0);
+  int minute_1_idx = (minute_idx -1 + r1_size ) % r1_size;
+  int minute_2_idx = (minute_idx +1           ) % r1_size;
+  int hour_idx   = (int)(0.5f+(time.hour%12 * 60 + time.minute) * r2_size / 720.0f)+r2_base;
+  int hour_1_idx = (hour_idx-r2_base -1 + r2_size ) % r2_size + r2_base;
+  int hour_2_idx = (hour_idx-r2_base +1           ) % r2_size + r2_base;
+
+  // set base colors
+  uint8 full = id(clock_brightness); 
+  uint8 dim = id(clock_brightness)/3; 
+  Color hour_color       = colors[0]*full;
+  Color minute_color     = colors[1]*full;
+  Color second_color     = colors[2]*full;
+  Color tick_color       = colors[3]*dim;
+  Color hour_arc_color   = hour_color * (uint8)(255/3);
+  Color minute_arc_color = minute_color * (uint8)(255/3);
+  Color second_arc_color = second_color * (uint8)(255/3);
 
   for (int i = 0; i < num_leds; i++) {
     if ( id(clock_fade_enabled) ) {
       int percentage = 98;
       clock_ring_colors[i] *= (uint8)(255*percentage/100);
     } else {
-      clock_ring_colors[i] = Color::BLACK;
+      if (id(clock_mode)==MODE_AINV) { // inverted
+        if ( i < r1_size ) {
+          clock_ring_colors[i] = minute_color;
+        } else {
+          clock_ring_colors[i] = hour_color;
+        }
+      } else {
+        clock_ring_colors[i] = Color::BLACK;
+      }
     }
   }
+  if (id(clock_mode)==MODE_AINV) { // inverted
+    minute_color= Color::BLACK;
+    hour_color= Color::BLACK;
+    tick_color= minute_color * (uint8)(2*255/3);
+  }
 
-  // set base colors
-  uint8 full = id(clock_brightness); 
-  uint8 dim = id(clock_brightness)/3; 
-  Color hour_color       = colors[0]*full; //Color(0xe11584)* full; //magenta
-  Color minute_color     = colors[1]*full; //Color(0xffd700)* full; //gold
-  Color second_color     = colors[2]*full; //Color(0xdc582a)* full; //dark orange
-  Color tick_color       = colors[3]*dim; //Color(0x555555)* full; //gray
-  Color hour_arc_color   = hour_color * (uint8)(255/3);
-  Color minute_arc_color = minute_color * (uint8)(255/3);
-  Color second_arc_color = second_color * (uint8)(255/3);
-
-  if (id(clock_mode)==2) { // arc from 0
+  if (id(clock_mode)==MODE_ARC12) { // arc from 0
     for (int i = 0; i < minute_idx; i++) {
       clock_ring_colors[i] = minute_arc_color;
     }
-    if (r2_size > 0 ) { 
-      for (int i = r1_size; i < hour_idx; i++) {
-        clock_ring_colors[i] = hour_arc_color;
-      }
-    } else {
-      for (int i = 0; i < hour_idx; i++) {
-        clock_ring_colors[i] = hour_arc_color;
-      }
-      if (minute_idx < hour_idx ) { //minutes overwritten, repaint them :(
+    for (int i = r2_base; i < hour_idx; i++) {
+      clock_ring_colors[i] = hour_arc_color;
+    }
+    if (r2_base == 0 ) { 
+      if (minute_idx < hour_idx ) { //minutes got overwritten, repaint them :(
         for (int i = 0; i < minute_idx; i++) {
-          clock_ring_colors[i] = minute_arc_color;
+            clock_ring_colors[i] = minute_arc_color;
         }
       }
     }
   }
-  if (id(clock_mode)==1) { // 5 min arc
+  if (id(clock_mode)==MODE_ARC5) { // 5 min arc
     int tick=r1_size/12;
-    int from= tick*(int)(second_idx/tick);
-    int to  = second_idx;
-    to  = tick*(int)((second_idx+tick-1)/tick);
-    if (from == to) { from--; to++; }
-    if (id(clock_seconds_enabled)) {
-      for (int i = from; i <= to; i++) {
-        clock_ring_colors[(i+r1_size)%r1_size] = second_arc_color;
-      }
-    }
-    from= tick*(int)(minute_idx/tick);
-    to  = tick*(int)((minute_idx+tick-1)/tick);
-    if (from == to) { from--; to++; }
+    int from= tick*(int)(minute_idx/tick);
+    int to  = tick*(int)((minute_idx+tick-1)/tick);
+    if (from == to) { from-=tick; to+=tick; }
     for (int i = from; i <= to; i++) {
       clock_ring_colors[(i+r1_size)%r1_size] = minute_arc_color;
     }
-    if (r2_size > 0 ) { 
+    if (r2_base > 0 ) { 
       tick=r2_size/12;
-      from= tick*(int)((hour_idx-r1_size)/tick);
-      to  = tick*(int)((hour_idx-r1_size+tick-1)/tick);
-      if (from == to) { from--; to++; }
+      from= tick*(int)((hour_idx-r2_base)/tick);
+      to  = tick*(int)((hour_idx-r2_base+tick-1)/tick);
+      if (from == to) { from-=tick; to+=tick; }
       for (int i = from; i <= to; i++) {
-        clock_ring_colors[(i+r2_size)%r2_size+r1_size] = hour_arc_color;
-      }
-    } else {
-      // will overwrite hour arc... no mixing
-      from= tick*(int)((hour_idx)/tick);
-      to  = tick*(int)((hour_idx+tick-1)/tick);
-      if (from == to) { from--; to++; }
-      for (int i = from; i <= to; i++) {
-        clock_ring_colors[(i+r1_size)%r1_size] = hour_arc_color;
+        clock_ring_colors[(i+r2_size)%r2_size+r2_base] = hour_arc_color;
       }
     }
   }
   
   // ticks and hands
-  if (id(clock_mode)==0) { // classic has ticks
+  if (id(clock_mode)==MODE_ANALOG) { // classic has ticks
     for (int i = 0; i < r1_size; i+=(int)(r1_size/12)) {
       clock_ring_colors[i] = tick_color;
     }
-    /*// 3h ticks on inner ring helps alignment
+    // 3h ticks on inner ring helps alignment
     for (int i = r1_size; i < num_leds; i+=(int)(r2_size/4)) {
       clock_ring_colors[i] = tick_color;
     }
-    */
+    
   }
   clock_ring_colors[hour_idx] = hour_color;
-  if (id(clock_mode)==0) { // classic has wide hour hand
+  if ((id(clock_mode)==MODE_AINV)) { // wide minute hand
+    clock_ring_colors[minute_1_idx] = minute_color;
+    clock_ring_colors[minute_2_idx] = minute_color;
+  }
+  if ((id(clock_mode)==MODE_ANALOG)||
+      (id(clock_mode)==MODE_ARC5)||
+      (id(clock_mode)==MODE_AINV)) { // wide hour hand
     clock_ring_colors[hour_1_idx] = hour_color;
     clock_ring_colors[hour_2_idx] = hour_color;
   }
@@ -154,13 +154,14 @@ esphome globals used:
 
   // apply clock_ring_colors to light
   int x=0;
+  //each ring has its own offset
   for (int i = 0; i < r1_size; i++) {
     x = (i + r1_size + offset[0]) % r1_size;
     it[x] = clock_ring_colors[i];
   }
-  if (r2_size > 0 ) {
-    for (int i = r1_size; i < num_leds; i++) {
-      x = (i - r1_size + offset[1]) % r2_size + r1_size;
+  if (r2_base > 0 ) {
+    for (int i = r2_base; i < num_leds; i++) {
+      x = (i - r2_base + offset[1]) % r2_size + r2_base;
       it[x] = clock_ring_colors[i];
     }
   }
